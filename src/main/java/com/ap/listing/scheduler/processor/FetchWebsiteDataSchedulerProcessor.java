5f129a3de0204e276c067d0cd90ec3747647e5c8
Rohit <rohitparih@gmail.com>
2025-07-12 17:45:35 +0530

    enhancement -> `fetch website scheduler implementation`

--- src/main/java/com/ap/listing/scheduler/processor/FetchWebsiteDataSchedulerProcessor.java
+++ src/main/java/com/ap/listing/scheduler/processor/FetchWebsiteDataSchedulerProcessor.java
@@ -11,15 +11,15 @@
  * <p>
  * You may not:
  * 1. Copy, modify, distribute, or sublicense this software without express
- * written permission from Advert Pulse or Bloggios.
+ *    written permission from Advert Pulse or Bloggios.
  * 2. Reverse engineer, decompile, disassemble, or otherwise attempt to derive
- * the source code of the software.
+ *    the source code of the software.
  * 3. Modify this license in any way, including but not limited to altering its
- * terms, even by Advert Pulse or any other entity, without express written
- * permission from Bloggios administrators. Bloggios is the creator of this
- * license and retains exclusive rights to update or modify it.
+ *    terms, even by Advert Pulse or any other entity, without express written
+ *    permission from Bloggios administrators. Bloggios is the creator of this
+ *    license and retains exclusive rights to update or modify it.
  * 4. Update or modify the license without written permission from Bloggios
- * administrators.
+ *    administrators.
  * <p>
  * The software is provided "as is," and Advert Pulse makes no warranties,
  * express or implied, regarding the software, including but not limited to any
@@ -98,102 +98,132 @@ public class FetchWebsiteDataSchedulerProcessor {
     )
     public void process(Scheduler scheduler) {
         log.info("{} >> {}", getClass().getSimpleName(), scheduler.toString());
-        String websiteId = scheduler.getPrimaryId();
-        Date now = new Date();
-        Optional<WebsiteData> websiteOptional = websiteRepository.findById(websiteId);
-        if (websiteOptional.isEmpty()) {
-            scheduler.setIsSchedulingDone(true);
-            scheduler.setScheduleCompletedOn(now);
-            scheduler.setUpdatedOn(now);
-            scheduler.setIsPassed(false);
-            scheduler.setMessage(MessageUtil.getMessage(scheduler.getMessage(), "Website is not found with the given primary id: " + websiteId));
-            Scheduler schedulerResponse = schedulerRepository.saveAndFlush(scheduler);
-            log.info("Scheduler saved successfully to database {}", schedulerResponse.toString());
-            return;
-        }
-        WebsiteData websiteData = websiteOptional.get();
-        String baseUrl = websiteData.getDomain();
-        String feignUrl;
-        if (baseUrl.startsWith(HTTPS)) {
-            feignUrl = baseUrl.substring(8);
-        } else {
-            feignUrl = baseUrl.substring(7);
-        }
-        CompletableFuture<ResponseEntity<DomainMetricsFeignResponse>> domainMetricsResponseFuture = CompletableFuture.supplyAsync(() -> domainMetricsFeignClient.getDomainMetrics(feignUrl));
-        CompletableFuture<ResponseEntity<String>> ahrefBacklinkResponseFuture = CompletableFuture.supplyAsync(() -> ahrefFeignClient.getBacklinkResponse(feignUrl));
-        CompletableFuture<ResponseEntity<String>> ahrefWebsiteTrafficResponseFuture = CompletableFuture.supplyAsync(() -> ahrefFeignClient.getWebsiteTraffic(feignUrl));
-        CompletableFuture<ResponseEntity<SimilarWebTrafficHistoryWrapper>> similarWebWebsiteTrafficResponseFuture = CompletableFuture.supplyAsync(() -> similarWebFeignClient.getWebsiteTraffic(feignUrl));
-        CompletableFuture.allOf(
-                domainMetricsResponseFuture,
-                ahrefBacklinkResponseFuture,
-                ahrefWebsiteTrafficResponseFuture,
-                similarWebWebsiteTrafficResponseFuture
-        );
-        ResponseEntity<DomainMetricsFeignResponse> domainMetricsResponse = domainMetricsResponseFuture.join();
-        ResponseEntity<String> ahrefBacklinkResponse = ahrefBacklinkResponseFuture.join();
-        ResponseEntity<String> ahrefWebsiteTrafficResponse = ahrefWebsiteTrafficResponseFuture.join();
-        ResponseEntity<SimilarWebTrafficHistoryWrapper> similarWebWebsiteTrafficResponse = similarWebWebsiteTrafficResponseFuture.join();
-        if (domainMetricsResponse.getStatusCode().is2xxSuccessful() && ahrefBacklinkResponse.getStatusCode().is2xxSuccessful() && ahrefWebsiteTrafficResponse.getStatusCode().is2xxSuccessful() && similarWebWebsiteTrafficResponse.getStatusCode().is2xxSuccessful()) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            DomainMetricsFeignResponse domainMetrics = domainMetricsResponse.getBody();
-            String ahrefBacklinkString = ahrefBacklinkResponse.getBody();
-            String ahrefWebsiteTrafficString = ahrefWebsiteTrafficResponse.getBody();
-            AhrefWebsiteAuthorityCheckerResponse ahrefBacklink = objectMapper.readValue(ahrefBacklinkString, AhrefWebsiteAuthorityCheckerResponse.class);
-            AhrefWebsiteTrafficResponse ahrefWebsiteTraffic = objectMapper.readValue(ahrefWebsiteTrafficString, AhrefWebsiteTrafficResponse.class);
-            SimilarWebTrafficHistoryWrapper similarWebWebsiteTraffic = similarWebWebsiteTrafficResponse.getBody();
-            boolean analyseDomainMetrics = analyseDomainMetrics(domainMetrics);
-            boolean analyseDomainRating = analyseDomainRating(ahrefBacklink);
-            websiteData.setMozDa(IntegerUtils.getOrZero(domainMetrics.getMozDA()));
-            websiteData.setMajesticTf(IntegerUtils.getOrZero(domainMetrics.getMajesticTF()));
-            websiteData.setDomainRating(ahrefBacklink.getOverview().getDomainRating());
-            if (!analyseDomainMetrics) {
-                websiteData.setIsActive(Boolean.FALSE.toString());
-                websiteData.setMessage("MozDA is less than 10");
+        try {
+            String websiteId = scheduler.getPrimaryId();
+            Date now = new Date();
+            Optional<WebsiteData> websiteOptional = websiteRepository.findById(websiteId);
+            if (websiteOptional.isEmpty()) {
+                scheduler.setIsSchedulingDone(true);
+                scheduler.setScheduleCompletedOn(now);
+                scheduler.setUpdatedOn(now);
+                scheduler.setIsPassed(false);
+                scheduler.setMessage(MessageUtil.getMessage(scheduler.getMessage(), "Website is not found with the given primary id: " + websiteId));
+                Scheduler schedulerResponse = schedulerRepository.saveAndFlush(scheduler);
+                log.info("Scheduler saved successfully to database {}", schedulerResponse.toString());
+                return;
             }
-            if (!analyseDomainRating) {
-                String message = websiteData.getMessage() != null ? websiteData.getMessage() : "";
-                websiteData.setIsActive(Boolean.FALSE.toString());
-                websiteData.setMessage("Domain Rating is less than 10 " + message);
+            WebsiteData websiteData = websiteOptional.get();
+            String baseUrl = websiteData.getDomain();
+            String feignUrl;
+            if (baseUrl.startsWith(HTTPS)) {
+                feignUrl = baseUrl.substring(8);
+            } else {
+                feignUrl = baseUrl.substring(7);
             }
-            if (analyseDomainMetrics && analyseDomainRating) {
-                websiteData.setIsActive(Boolean.TRUE.toString());
+            CompletableFuture<ResponseEntity<DomainMetricsFeignResponse>> domainMetricsResponseFuture = CompletableFuture.supplyAsync(() -> domainMetricsFeignClient.getDomainMetrics(feignUrl));
+            CompletableFuture<ResponseEntity<String>> ahrefBacklinkResponseFuture = CompletableFuture.supplyAsync(() -> ahrefFeignClient.getBacklinkResponse(feignUrl));
+            CompletableFuture<ResponseEntity<String>> ahrefWebsiteTrafficResponseFuture = CompletableFuture.supplyAsync(() -> ahrefFeignClient.getWebsiteTraffic(feignUrl));
+            CompletableFuture<ResponseEntity<SimilarWebTrafficHistoryWrapper>> similarWebWebsiteTrafficResponseFuture = CompletableFuture.supplyAsync(() -> similarWebFeignClient.getWebsiteTraffic(feignUrl));
+            CompletableFuture.allOf(
+                    domainMetricsResponseFuture,
+                    ahrefBacklinkResponseFuture,
+                    ahrefWebsiteTrafficResponseFuture,
+                    similarWebWebsiteTrafficResponseFuture
+            );
+            ResponseEntity<DomainMetricsFeignResponse> domainMetricsResponse = domainMetricsResponseFuture.join();
+            ResponseEntity<String> ahrefBacklinkResponse = ahrefBacklinkResponseFuture.join();
+            ResponseEntity<String> ahrefWebsiteTrafficResponse = ahrefWebsiteTrafficResponseFuture.join();
+            ResponseEntity<SimilarWebTrafficHistoryWrapper> similarWebWebsiteTrafficResponse = similarWebWebsiteTrafficResponseFuture.join();
+            if (domainMetricsResponse.getStatusCode().is2xxSuccessful() && ahrefBacklinkResponse.getStatusCode().is2xxSuccessful() && ahrefWebsiteTrafficResponse.getStatusCode().is2xxSuccessful() && similarWebWebsiteTrafficResponse.getStatusCode().is2xxSuccessful()) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                DomainMetricsFeignResponse domainMetrics = domainMetricsResponse.getBody();
+                String ahrefBacklinkString = ahrefBacklinkResponse.getBody();
+                String ahrefWebsiteTrafficString = ahrefWebsiteTrafficResponse.getBody();
+                AhrefWebsiteAuthorityCheckerResponse ahrefBacklink = objectMapper.readValue(ahrefBacklinkString, AhrefWebsiteAuthorityCheckerResponse.class);
+                AhrefWebsiteTrafficResponse ahrefWebsiteTraffic = objectMapper.readValue(ahrefWebsiteTrafficString, AhrefWebsiteTrafficResponse.class);
+                SimilarWebTrafficHistoryWrapper similarWebWebsiteTraffic = similarWebWebsiteTrafficResponse.getBody();
+                boolean analyseDomainMetrics = analyseDomainMetrics(domainMetrics);
+                boolean analyseDomainRating = analyseDomainRating(ahrefBacklink);
+                websiteData.setMozDa(IntegerUtils.getOrZero(domainMetrics.getMozDA()));
+                websiteData.setMajesticTf(IntegerUtils.getOrZero(domainMetrics.getMajesticTF()));
+                websiteData.setDomainRating(ahrefBacklink.getOverview().getDomainRating());
+                if (!analyseDomainMetrics) {
+                    websiteData.setIsActive(Boolean.FALSE.toString());
+                    websiteData.setMessage("MozDA is less than 10");
+                }
+                if (!analyseDomainRating) {
+                    String message = websiteData.getMessage() != null ? websiteData.getMessage() : "";
+                    websiteData.setIsActive(Boolean.FALSE.toString());
+                    websiteData.setMessage("Domain Rating is less than 10 " + message);
+                }
+                if (analyseDomainMetrics && analyseDomainRating) {
+                    websiteData.setIsActive(Boolean.TRUE.toString());
+                }
+                websiteData.setAhrefOrganicTraffic(ahrefWebsiteTraffic.getTraffic().getTrafficMonthlyAvg());
+                websiteData.setSimilarWebTraffic(similarWebWebsiteTraffic.getDomainAnalytics().getEngagements().getVisits());
+                websiteData.setUrlRating(ahrefBacklink.getOverview().getUrlRating());
+                websiteData.setCountryCode(similarWebWebsiteTraffic.getDomainAnalytics().getCountryRank().getCountryCode());
+                websiteData.setCountry(CountryNameUtil.getCountryName(similarWebWebsiteTraffic.getDomainAnalytics().getCountryRank().getCountryCode()));
+                websiteData.setAhrefTrafficHistory(ahrefWebsiteTraffic.getTrafficHistory());
+                websiteData.setAhrefTopCountries(ahrefWebsiteTraffic.getTopCountries());
+                websiteData.setSimilarWebTrafficHistory(processSimilarWebTraffic(similarWebWebsiteTraffic));
+                websiteData.setSimilarWebTopCountries(processTopCountry(similarWebWebsiteTraffic));
+                WebsiteData websiteDataResponse = websiteRepository.save(websiteData);
+                log.info("Website Data Saved Successfully {} ", websiteDataResponse.toString());
+                scheduler.setScheduleCompletedOn(now);
+                scheduler.setIsSchedulingDone(Boolean.TRUE);
+                scheduler.setUpdatedOn(now);
+                scheduler.setIsPassed(Boolean.TRUE);
+                Scheduler schedulerResponse = schedulerRepository.save(scheduler);
+                log.info("Scheduler Saved Successfully {} ", schedulerResponse.toString());
+            } else {
+                scheduler.setMessage(
+                        MessageUtil.getMessage(
+                                scheduler.getMessage(),
+                                String.format("Feign Response Error for schedulerId: %s domainMetricsResponse: %s, ahrefBacklinkResponse: %s, ahrefWebsiteTrafficResponse: %s and similarWebWebsiteTrafficResponse: %s",
+                                        scheduler.getSchedulerId(),
+                                        domainMetricsResponse.getStatusCode(),
+                                        ahrefBacklinkResponse.getStatusCode(),
+                                        ahrefWebsiteTrafficResponse.getStatusCode(),
+                                        similarWebWebsiteTrafficResponse.getStatusCode()
+                                )
+                        )
+                );
+                log.error(
+                        "DomainMetricsFeignResponse: {}, AhrefBacklinkResponse: {}, AhrefTrafficResponse: {}, SimilarWebTrafficResponse: {}",
+                        domainMetricsResponse.getBody(),
+                        ahrefBacklinkResponse.getBody(),
+                        ahrefWebsiteTrafficResponse.getBody(),
+                        similarWebWebsiteTrafficResponse.getBody()
+                );
+                if (scheduler.getTimesUsed() <= limit) {
+                    log.info("Scheduler Put on retry mode for Website Fetch Data Feign Response");
+                    int timesUsed = scheduler.getTimesUsed();
+                    scheduler.setTimesUsed(timesUsed + 1);
+                    scheduler.setScheduledOn(DateUtils.addHours(now, timesUsed * multiplier));
+                    scheduler.setUpdatedOn(now);
+                    Scheduler schedulerResponse = schedulerRepository.save(scheduler);
+                    log.info("Scheduler saved successfully to database {}", schedulerResponse.toString());
+                } else {
+                    scheduler.setIsSchedulingDone(true);
+                    scheduler.setUpdatedOn(now);
+                    scheduler.setIsPassed(Boolean.FALSE);
+                    scheduler.setScheduleCompletedOn(now);
+                    Scheduler schedulerResponse = schedulerRepository.save(scheduler);
+                    log.info("Scheduler saved successfully to database {}", schedulerResponse.toString());
+                }
             }
-            websiteData.setAhrefOrganicTraffic(ahrefWebsiteTraffic.getTraffic().getTrafficMonthlyAvg());
-            websiteData.setSimilarWebTraffic(similarWebWebsiteTraffic.getDomainAnalytics().getEngagements().getVisits());
-            websiteData.setUrlRating(ahrefBacklink.getOverview().getUrlRating());
-            websiteData.setCountryCode(similarWebWebsiteTraffic.getDomainAnalytics().getCountryRank().getCountryCode());
-            websiteData.setCountry(CountryNameUtil.getCountryName(similarWebWebsiteTraffic.getDomainAnalytics().getCountryRank().getCountryCode()));
-            websiteData.setAhrefTrafficHistory(ahrefWebsiteTraffic.getTrafficHistory());
-            websiteData.setAhrefTopCountries(ahrefWebsiteTraffic.getTopCountries());
-            websiteData.setSimilarWebTrafficHistory(processSimilarWebTraffic(similarWebWebsiteTraffic));
-            websiteData.setSimilarWebTopCountries(processTopCountry(similarWebWebsiteTraffic));
-            WebsiteData websiteDataResponse = websiteRepository.save(websiteData);
-            log.info("Website Data Saved Successfully {} ", websiteDataResponse.toString());
-            scheduler.setScheduleCompletedOn(now);
-            scheduler.setIsSchedulingDone(Boolean.TRUE);
-            scheduler.setUpdatedOn(now);
-            scheduler.setIsPassed(Boolean.TRUE);
-            Scheduler schedulerResponse = schedulerRepository.save(scheduler);
-            log.info("Scheduler Saved Successfully {} ", schedulerResponse.toString());
-        } else {
+        } catch (RuntimeException exception) {
+            Date now = new Date();
             scheduler.setMessage(
                     MessageUtil.getMessage(
                             scheduler.getMessage(),
-                            String.format("Feign Response Error for schedulerId: %s domainMetricsResponse: %s, ahrefBacklinkResponse: %s, ahrefWebsiteTrafficResponse: %s and similarWebWebsiteTrafficResponse: %s",
-                                    scheduler.getSchedulerId(),
-                                    domainMetricsResponse.getStatusCode(),
-                                    ahrefBacklinkResponse.getStatusCode(),
-                                    ahrefWebsiteTrafficResponse.getStatusCode(),
-                                    similarWebWebsiteTrafficResponse.getStatusCode()
-                            )
+                            exception.getLocalizedMessage()
                     )
             );
             log.error(
-                    "DomainMetricsFeignResponse: {}, AhrefBacklinkResponse: {}, AhrefTrafficResponse: {}, SimilarWebTrafficResponse: {}",
-                    domainMetricsResponse.getBody(),
-                    ahrefBacklinkResponse.getBody(),
-                    ahrefWebsiteTrafficResponse.getBody(),
-                    similarWebWebsiteTrafficResponse.getBody()
+                    "Runtime Exception Occurred while processing the Fetch Website Data",
+                    exception
             );
             if (scheduler.getTimesUsed() <= limit) {
                 log.info("Scheduler Put on retry mode for Website Fetch Data Feign Response");
