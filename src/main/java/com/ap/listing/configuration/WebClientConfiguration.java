ffce9122adc4b41f2a7af7e881fd0fd28678d531
Rohit <rohitparih@gmail.com>
2025-07-23 22:51:01 +0530

    implementation -> `Verify Ownership Upload Bug`

--- src/main/java/com/ap/listing/configuration/WebClientConfiguration.java
+++ src/main/java/com/ap/listing/configuration/WebClientConfiguration.java
@@ -39,6 +39,7 @@ package com.ap.listing.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -46,6 +47,12 @@ public class WebClientConfiguration {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(10 * 1024 * 1024)
+                        ).build())
+                .build();
     }
 }
